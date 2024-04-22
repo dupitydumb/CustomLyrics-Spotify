e15d63e9a6ab045745f06603f6d7b469ae99678a
AvKov <37872714+dupitydumb@users.noreply.github.com>
2024-04-22 17:17:51 +0700

    Fix Wont load when song change

--- LyricsPlus/content.js
+++ LyricsPlus/content.js
@@ -11,6 +11,31 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     ActivateLyrics();
   }
 });
+
+if (window.location.href.includes("spotify.com")) {
+  // Select the node that will be observed for mutations
+  var targetNode = document.body;
+
+  // Options for the observer (which mutations to observe)
+  var config = { attributes: true, childList: true, subtree: true };
+
+  // Callback function to execute when mutations are observed
+  var callback = function (mutationsList, observer) {
+    for (let mutation of mutationsList) {
+      if (mutation.type === "childList") {
+        // Run your code here
+        ActivateLyrics();
+      }
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  var observer = new MutationObserver(callback);
+
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+}
+
 function ActivateLyrics() {
   chrome.storage.sync.get(
     {
